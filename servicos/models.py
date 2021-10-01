bb09f7865a6eedff8d9b6e4042b55dfbfa2fe716
Lucas Almeida <v.francozo@hotmail.com>
2021-10-01 14:46:50 -0300

    servicos update

--- servicos/models.py
+++ servicos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cadastro.models import Voluntario,Idoso
+from cadastro.models import Voluntario, Idoso
 
 # Create your models here.
 
--- servicos/views.py
+++ servicos/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .forms import ServicoModel
 
 # Create your views here.
 
 def solicitar_servico(request):
-    return render(request, 'solicitar_servico.html')
+    user = request.user
+    if request.method == 'POST':
+        form = ServicoModel(request.POST)
+        if form.is_valid():
+            print(form)
+            form.save()
+    else:
+        form = ServicoModel()
+
+    return render(request, 'solicitar_servico.html',{'form':form})
 
