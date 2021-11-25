74c55ef985781b0b218bd7b74f42acc4bd96beb6
Vinicius Françozo <v.francozo@hotmail.com>
2021-11-25 07:58:32 -0300

    iniciar + encerrar serviços
    Adicionada as funções de Iniciar Serviço, no perfil de voluntário e Encerrar Serviço, no perfil de Idoso.

--- servicos/urls.py
+++ servicos/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns =[
     path('solicitar/', views.solicitar_servico, name='solicitar'),
     path('meus_servicos/', views.meus_servicos, name='meus_servicos'),
-    path('confirmar/<int:servico_id>', views.confirmar, name='confirmar')
+    path('meus_servicos/voluntario', views.meus_servicos_voluntario, name='meus_servicos_voluntario'),
+    path('iniciar/<int:servico_id>', views.iniciar, name='iniciar'),
+    path('confirmar/<int:servico_id>', views.confirmar, name='confirmar'),
+    path('encerrar/<int:servico_id>', views.encerrar, name='encerrar')
 ]
--- servicos/views.py
+++ servicos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ServicoModel
 from cadastro.models import Idoso, Account, Voluntario
 from .models import ServicoSolicitado
+import datetime
 
 # Create your views here.
 
@@ -42,6 +43,25 @@ def meus_servicos(request):
 
     return render(request, 'meus_servicos.html', context)
 
+def meus_servicos_voluntario(request):
+    voluntario = request.user.voluntario
+
+    aguardando = ServicoSolicitado.objects.filter(voluntario_id=voluntario.id).exclude(confirmado=True)
+    em_andamento = ServicoSolicitado.objects.filter(voluntario_id=voluntario.id).exclude(confirmado=False).exclude(data_fim__isnull=False)
+    concluidos = ServicoSolicitado.objects.filter(voluntario_id=voluntario.id).exclude(data_fim__isnull=True).exclude(data_inicio__isnull=True)
+
+    context = {
+        'aguardando': {},
+        'em_andamento': em_andamento,
+        'concluidos': concluidos
+    }
+
+    for i in aguardando:
+        v = Idoso.objects.get(id=i.idoso_id_id)
+        context['aguardando'][i] = Account.objects.get(id=v.idoso_id)
+
+    return render(request, 'meus_servicos_voluntario.html', context)
+
 def confirmar(request, servico_id):
     servico = get_object_or_404(ServicoSolicitado, pk=servico_id)
     idoso = Account.objects.filter(email=servico.idoso_id)[0]
@@ -58,3 +78,35 @@ def confirmar(request, servico_id):
         return redirect('index')
 
     return render(request, 'confirmar.html', context)
+
+
+def iniciar(request, servico_id):
+    servico = get_object_or_404(ServicoSolicitado, pk=servico_id)
+    v = Idoso.objects.get(id=servico.idoso_id_id)
+    context = {
+        'idoso': Account.objects.get(id=v.idoso_id),
+        'servico': servico
+    }
+    print(context['idoso'])
+
+    if request.method == 'POST':
+        servico.data_inicio = datetime.datetime.today()
+        servico.hora_inicio = datetime.datetime.now().strftime("%H:%M:%S")
+        servico.save(update_fields=['data_inicio', 'hora_inicio'])
+        return redirect('index')
+
+    return render(request, 'iniciar.html', context)
+
+
+def encerrar(request, servico_id):
+    servico = get_object_or_404(ServicoSolicitado, pk=servico_id)
+    context = {
+        "servico": servico
+    }
+    if request.method == 'POST':
+        servico.data_fim = datetime.datetime.today()
+        servico.hora_fim = datetime.datetime.now().strftime("%H:%M:%S")
+        servico.save(update_fields=['data_fim', 'hora_fim'])
+        return redirect('index')
+
+    return render(request, 'encerrar.html', context)
